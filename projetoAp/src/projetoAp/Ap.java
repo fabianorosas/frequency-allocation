c809c3f4aca28a611cdafe561dcbcb142d029257
fabiano <fabianorosas@gmail.com>
2014-04-22 15:36:11 -0300

    Some message passing. Core logic still missing.

--- projetoAp/src/projetoAp/Ap.java
+++ projetoAp/src/projetoAp/Ap.java
@@ -11,19 +11,26 @@ public class Ap extends Host {
 	
 	private static final int CHANNEL_SWITCHING_DELAY = 1000;
 	private static final int CHANNEL_SWITCHING_PERIOD = 1000;
-	
-	private int psi;
+
 	private Timer timer;
 	private TimerTask switchChannel;
+	
+	private int psi;
+	private int channel;
+
 	
 	public Ap(String clients, int port) throws SocketException{
 		super(clients, port);
 		this.psi = CAN_SWITCH;
+		this.channel = 1; //TODO: 1?
 		
 		startTimer();
 		listen();
 	}
 
+	/**
+	 * Executes the channel switching logic every <CHANNEL_SWITCHING_PERIOD> milliseconds.
+	 */
 	private void startTimer(){
 		timer = new Timer();
 		switchChannel = new TimerTask(){
@@ -38,51 +45,63 @@ public class Ap extends Host {
 		this.timer.schedule(switchChannel, CHANNEL_SWITCHING_DELAY, CHANNEL_SWITCHING_PERIOD);
 	}
 	
+	/**
+	 * Corresponds to the part where the AP functions as a client.
+	 */
 	private void listen(){
 		msg = receiveMessage();
 		if( msg.startsWith("#lock") ) { 
-			//	TODO: locking logic
+			if(canBeLocked()){
+				psi++; //lock
+				replyWithChannel();
+			}
 		}
 		else if( msg.startsWith("#unlock") ){
-			//	TODO: unlocking logic
+			psi--;
+		}
+		else if ( msg.startsWith("#c") ){
+			//TODO: we tried to lock client and client replied "OK, here´s the channel I'm at."
+			//TODO: Ex: #c11, #c6, #c1
 		}
 		else{
 			System.err.println("Stray message: " + msg);
 		}
-		
 	}	
 	
 	private void startPhase1(){
 		this.psi = BUSY_SWITCHING;
-		
-		lockAllClients();
+		sendBroadcast("#lock");
 	}
 	
 	private void startPhase2(){
 		waitForReplies();
 		if(allChildrenAreLocked()){
-			switchChannel();
+			tryToSwitchChannel();
 		}
-		unlockAll();
+		unlockAllChildren();
 	}
 
 	private void waitForReplies(){
-		
+		//TODO: find a way to block the AP until all clients respond.
+		//TODO: should it really block? or should it keep listening to the medium and receiving locking requests from other APs?
 	}
 
-	private void switchChannel(){
-		
+	private void replyWithChannel(){
+		String toSend = "#" + channel; 
+		sendMessage(toSend, dtgReceive.getAddress().getHostAddress(), dtgReceive.getPort());
 	}
 	
-	private void lockAllClients() {
-		sendBroadcast("#lock");
+	private void tryToSwitchChannel(){
+		//TODO: the channel switching logic
 	}
 	
-	private void unlockAll(){
-		
+	private void unlockAllChildren(){
+		sendBroadcast("#unlock");
+		//TODO: send this message only to the APs that were previously locked, to reduce network overhead
 	}
 	
 	private boolean allChildrenAreLocked(){
+		//TODO: check the response of the children to know if they were able to lock.
 		return false;
 	}
 	
