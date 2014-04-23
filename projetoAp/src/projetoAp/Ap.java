ba6dd20d9ace7fe6711e4e24e55eb5536025fd7e
fabiano <fabianorosas@gmail.com>
2014-04-23 11:31:45 -0300

    Waiting for replies and client list logic

--- projetoAp/src/projetoAp/Ap.java
+++ projetoAp/src/projetoAp/Ap.java
@@ -1,6 +1,7 @@
 package projetoAp;
 
 import java.net.SocketException;
+import java.util.Arrays;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -11,18 +12,24 @@ public class Ap extends Host {
 	
 	private static final int CHANNEL_SWITCHING_DELAY = 1000;
 	private static final int CHANNEL_SWITCHING_PERIOD = 1000;
+	private int NUMBER_OF_CLIENTS;
 
 	private Timer timer;
 	private TimerTask switchChannel;
 	
 	private int psi;
 	private int channel;
-
+	private int[] clientResponse;
+	private String[] clients;
 	
 	public Ap(String clients, int port) throws SocketException{
 		super(clients, port);
+
+		countClients();
+		initClientsResponse();
+		
 		this.psi = CAN_SWITCH;
-		this.channel = 1; //TODO: 1?
+		this.channel = 1;
 		
 		startTimer();
 		listen();
@@ -42,7 +49,7 @@ public class Ap extends Host {
 				}
 			}
 		};
-		this.timer.schedule(switchChannel, CHANNEL_SWITCHING_DELAY, CHANNEL_SWITCHING_PERIOD);
+		this.timer.schedule(switchChannel, CHANNEL_SWITCHING_DELAY, CHANNEL_SWITCHING_PERIOD); //TODO: randomize period
 	}
 	
 	/**
@@ -56,15 +63,15 @@ public class Ap extends Host {
 				reply("#" + channel);
 			}
 			else{
-				reply("#busy");
+				reply("#c" + BUSY_SWITCHING );
 			}
 		}
 		else if( msg.startsWith("#unlock") ){
 			psi--;
 		}
 		else if ( msg.startsWith("#c") ){
-			//TODO: we tried to lock client and client replied "OK, here´s the channel I'm at."
-			//TODO: Ex: #c11, #c6, #c1
+			int clientIndex = Arrays.binarySearch(clients, dtgReceive.getAddress().getHostAddress() + ":" + dtgReceive.getPort() + "#");
+			clientResponse[clientIndex] = Integer.parseInt(msg.substring(2));
 		}
 		else{
 			System.err.println("Stray message: " + msg);
@@ -73,7 +80,7 @@ public class Ap extends Host {
 	
 	private void startPhase1(){
 		this.psi = BUSY_SWITCHING;
-		sendBroadcast("#lock");
+		lockAllChildren();
 	}
 	
 	private void startPhase2(){
@@ -85,16 +92,29 @@ public class Ap extends Host {
 	}
 
 	private void waitForReplies(){
-		//TODO: find a way to block the AP until all clients respond.
-		//TODO: should it really block? or should it keep listening to the medium and receiving locking requests from other APs?
+		while(!allChildrenReplied());
 	}
 
+	private boolean allChildrenReplied(){
+		for(int response : clientResponse){
+			if(response == -2){
+				return false;
+			}
+		}
+		return true;
+	}
+	
 	private void reply(String toSend){
 		sendMessage(toSend, dtgReceive.getAddress().getHostAddress(), dtgReceive.getPort());
 	}
 	
 	private void tryToSwitchChannel(){
 		//TODO: the channel switching logic
+	}
+	
+	private void lockAllChildren(){
+		resetClientResponse();
+		sendBroadcast("#lock");
 	}
 	
 	private void unlockAllChildren(){
@@ -105,6 +125,21 @@ public class Ap extends Host {
 	private boolean allChildrenAreLocked(){
 		//TODO: check the response of the children to know if they were able to lock.
 		return false;
+	}
+	
+	private void countClients(){
+		clients = clientList.split("#");
+		Arrays.sort(clients);
+		NUMBER_OF_CLIENTS = clients.length;
+	}
+		
+	private void initClientsResponse(){
+		clientResponse = new int[NUMBER_OF_CLIENTS];
+		resetClientResponse();
+	}
+	
+	private void resetClientResponse(){
+		Arrays.fill(clientResponse, -2);
 	}
 	
 	private boolean canBeLocked(){
