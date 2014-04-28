55a892a94f0277f6957289b7c89940fb52c25f3d
fabiano <fabianorosas@gmail.com>
2014-04-28 14:38:43 -0300

    Turned main into a server that sets the topology.

--- projetoAp/src/projetoAp/Main.java
+++ projetoAp/src/projetoAp/Main.java
@@ -8,59 +8,73 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Main {
+public class Main extends Host {
 	
 	private static int NUMBER_OF_APS;
 	private static ArrayList<char []> topology = new ArrayList<char []>();
-	private static ArrayList<Ap> network = new ArrayList<Ap>();
 	private static Map<Integer,String> apAddrs = new HashMap<Integer, String>();
 	
-	public static void main (String[] args) throws FileNotFoundException, SocketException{
-		Scanner file = new Scanner(new FileReader(args[0]));
-
-		for(int i = 0; file.hasNext(); i++){
-			topology.add(file.nextLine().toCharArray());
-			mapTopology(i, instantiateAp());
-		}
-		file.close();
-		
-		NUMBER_OF_APS = topology.size();
-		
-		setupClientLists();
-		startAps();
+	public Main(int port) throws SocketException{
+		super(port);
+		setupTopology();
 	}
 	
-	private static void mapTopology(int idx, Ap ap){
-		apAddrs.put(idx, ap.getSocket().getLocalAddress().getHostAddress() + ":" + ap.getSocket().getLocalPort());
+	private void setupTopology() throws SocketException{
+		initAps();
+		waitForReplies();
+		setupClientLists();		
 	}
 	
-	private static Ap instantiateAp() throws SocketException{
-		Ap ap = new Ap();
-		network.add(ap);
-		return ap;
-	}
-	
-	private static void setupClientLists(){
+	private void initAps() throws SocketException {
 		for(int ap=0; ap < NUMBER_OF_APS; ap++){
-			network.get(ap).setNUMBER_OF_CLIENTS(NUMBER_OF_APS);
-			network.get(ap).setClientList(makeClientList(ap));
+			Ap.main(new String[]{String.valueOf(ap)});
+		}
+	}
+	
+	public void waitForReplies() {
+		int clientIdx;
+		int numberOfReplies = 0;
+		
+		while(true){
+			msg = receiveMessage();
+			if( msg.startsWith("#") ) {
+				clientIdx = Integer.parseInt(msg.substring(1));
+				apAddrs.put(clientIdx, dtgReceive.getAddress().getHostAddress() + ":" + dtgReceive.getPort());
+				numberOfReplies++;
+				if(numberOfReplies == NUMBER_OF_APS)
+					break;
+			}
+		}
+	}
+	
+	private void setupClientLists(){
+		String[] apAddr;
+		
+		for(int ap=0; ap < NUMBER_OF_APS; ap++){
+			apAddr = apAddrs.get(ap).split(":");
+			sendMessage("#clientList#" + makeClientList(ap), apAddr[0], Integer.parseInt(apAddr[1]));
 		}		
 	}
 	
-	private static ArrayList<String> makeClientList(int ap){
-		ArrayList<String> clientList = new ArrayList<String>(NUMBER_OF_APS);
+	private static String makeClientList(int ap){
+		String clientList = new String();
 			
-		for(int i=0; i < NUMBER_OF_APS; i++){
+		for(int i=0; i < NUMBER_OF_APS;	 i++){
 			if(topology.get(ap)[i] == '1'){
-				clientList.add(apAddrs.get(i));
+				clientList.concat(apAddrs.get(i) + "#");
 			}
 		}
 		return clientList;
 	}
-	
-	private static void startAps(){
-		for(Ap ap : network){
-			ap.start();
+
+	public static void main (String[] args) throws FileNotFoundException, SocketException{
+	Scanner file = new Scanner(new FileReader(args[0]));
+
+		while(file.hasNext()){
+			topology.add(file.nextLine().toCharArray());
 		}
+		file.close();
+		
+		NUMBER_OF_APS = topology.size();
 	}
 }
